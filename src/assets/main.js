13262328ab4f51835a3680e1f54932e2a622d143
bhavya2810 <bhavyadhiman7@gmail.com>
2017-12-18 17:49:05 +0530

    added div course

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
-
-  // your code will go here
+   $.ajax({
+      url:'https://www.codeschool.com/users/bhavya_1.json',
+      dataType:'jsonp',
+      success:function(response){
+        for(let i = 0; i<response.courses.completed.length;i++)
+        {
+           $("#badges").add(`<div class='course'></div>`);
+        }
+      }
+   });
 
 });
