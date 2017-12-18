d714d26937eb3a39c4e50f34e2cf6bc338499157
bhavya2810 <bhavyadhiman7@gmail.com>
2017-12-18 18:15:19 +0530

    completed project...yayyyy

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,6 +8,7 @@ $(function() {
            $("#badges").append(`<div class='course'>
            <h3>${response.courses.completed[i].title}</h3>
            <img src=${response.courses.completed[i].badge}>
+           <a href=${response.courses.completed[i].url} target='_blank' class ='btn btn-primary'>See Course</a>
            </div>`);
         }
       }
